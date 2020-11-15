c89a6adf178d0d6601b413dd005d18c3f898a251
Arthur Leblanc <arthurleblanc11@gmail.com>
2020-11-15 04:10:00 +0100

    feat(new_contamination): add signal positive post route for users

--- src/main/java/polytech/group3/iwa/alert_contact_case/controllers/UserController.java
+++ src/main/java/polytech/group3/iwa/alert_contact_case/controllers/UserController.java
@@ -1,9 +1,16 @@
 package polytech.group3.iwa.alert_contact_case.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import polytech.group3.iwa.alert_contact_case.models.CaseType;
+import polytech.group3.iwa.alert_contact_case.models.CovidInfo;
+import polytech.group3.iwa.alert_contact_case.repositories.CaseTypeRepository;
+import polytech.group3.iwa.alert_contact_case.repositories.CovidInfoRepository;
+import polytech.group3.iwa.alert_contact_case.repositories.UserRepository;
 
 @RestController
 @RequestMapping("api/users")
@@ -11,6 +18,15 @@ public class UserController {
 
     @Autowired
     public JavaMailSender emailSender;
+
+    @Autowired
+    private CaseTypeRepository caseTypeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CovidInfoRepository covidInfoRepository;
 
     @PostMapping
     @RequestMapping("/notify")
@@ -23,6 +39,32 @@ public class UserController {
         message.setText("Hello, you have recently added a location to our application. This location was recently frequented by a person positive to covid-19. You are now considered as a contact case. Please, be tested as soon as possible.");
         this.emailSender.send(message);
 
+    }
+
+    @PostMapping
+    @RequestMapping("/declarePositive")
+    public CovidInfo newContamination(@RequestHeader (name="Authorization") String token, @RequestBody CovidInfo covidInfo) {
+        // TODO : Implement Token verification
+
+        //Check that all the required fields are not null
+        if (!isValidCovidInfo(covidInfo)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //Check if the specified case type exists in the database
+        if(!caseTypeRepository.existsById(covidInfo.getId_case_type())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //Check if the specified user exists in the database
+        if (!userRepository.existsById(covidInfo.getId_keycloak())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //Save a new covid case in the database
+        return covidInfoRepository.saveAndFlush(covidInfo);
+    }
+
+    public boolean isValidCovidInfo(CovidInfo covidInfo) {
+        return covidInfo.getId_case_type() != 0 && covidInfo.getId_case_type() != 0 && covidInfo.getReporting_date() != null;
     }
 
 }
